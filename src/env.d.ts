e46a3772676a65146e31c0779eaf8e6958d8fa7f
phasenull <55253035+phasenull@users.noreply.github.com>
2025-07-03 10:48:22 +0300

    no need to do encryption stuff

--- src/env.d.ts
+++ src/env.d.ts
@@ -17,7 +17,7 @@ declare module "bun" {
 		// api-key for email provider (you should probably see email.service.ts for more info)
 		KEY_EMAIL_SERVICE?: string
 
-		// secret key to handle encrypted communication between internal service and public service
+		// secret key to handle communication between internal service and public service
 		INTERNAL_COMM_SECRET?: string
 		// set this to true if you weren't informed about any internal services and just wanna host
 		// your own gezcez.com instance.
